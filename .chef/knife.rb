26a69eaf5375cf909198097707b2bd8e126f0f03
nitesh <nitesh.gupta01@yahoo.in>
2016-05-02 07:57:41 +0530

    2nd

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,11 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "niteshgupta1"
+client_key               "#{current_dir}/niteshgupta1.pem"
+validation_client_name   "nitesh-validator"
+validation_key           "#{current_dir}/nitesh-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/nitesh"
+cookbook_path            ["#{current_dir}/../cookbooks"]
---
+++ cookbooks/test/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: test
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+file "/tmp/nitesh.txt" do
+	content "Hi this file created for testing"
+end
+
+file "/tmp/nitesh.txt" do
+     action :delete
+end
